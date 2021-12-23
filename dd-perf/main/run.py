55eccbdaf1bbaddc5494432cc62b840ddcb863fb
jarodabel <jarod@dronedeploy.com>
2021-12-22 20:59:46 -0600

    a chart

--- dd-perf/main/run.py
+++ dd-perf/main/run.py
@@ -1,8 +1,15 @@
-
 from pandas import read_csv
+from matplotlib import pyplot as plt
 
-data = read_csv('./main/data.csv')
-res = data.loc[data['environment'] == 'prod']
-df = res.groupby(['version', 'environment', 'path_group','date']).sum()
+data = read_csv("./main/data.csv")
+prod_data = data.loc[data["environment"] == "prod"]
+prod_data_grouped = (
+    prod_data.filter(items=["date", "version", "total_locks"])
+    .groupby(["version", "date"])
+    .agg(["sum", "count"])
+)
 
-print(df)
+ax = prod_data_grouped.plot()
+
+fig = ax.get_figure()
+fig.savefig('/main/figure.png')
